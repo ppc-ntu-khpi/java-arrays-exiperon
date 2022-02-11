cfbd49c93df794e68b1661fd9be69178e27d6033
exiperon <67110285+exiperon@users.noreply.github.com>
2022-02-11 15:52:20 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -3,10 +3,7 @@ package test;
 import domain.Exercise;
 
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        System.out.println("Комбінацій для підіймання по сходах: " + Exercise.Calculate());
     }
 }
