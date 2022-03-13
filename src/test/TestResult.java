2c1a5259f3397b392db972dcee01211921df8533
exiperon <67110285+exiperon@users.noreply.github.com>
2022-03-13 16:58:11 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,12 +2,11 @@ package test;
 
 import domain.Exercise;
 
-import java.util.Arrays;
 
 public class TestResult {
 
     public static void main(String[] args) {
-        char[] equation = "1 + 33-4 * 7".toCharArray();
-        System.out.println(Arrays.toString(equation) + " = " + Exercise.calculate(equation));
+        String equation = "1 + 33-4 * 7";
+        System.out.println(equation + " = " + Exercise.calculate(equation));
     }
 }
